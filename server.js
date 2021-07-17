a668dd5ae5e73e208c6e599f3185d3b5b2bf3076
bartkowiakmateusz <bartkowiakmateusz@gmail.com>
2021-07-17 20:21:07 +0200

    1 file modified

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http
   .createServer(function(req, res) {
-    res.write('Hello, Node.js 45646!'); //write a response to the client
+    res.write('Hello, Node.js 646!'); //write a response to the client
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
